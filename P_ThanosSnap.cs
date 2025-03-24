67a58a43f2b88dd43cdf1706111a084aad514f82
cmSalticidae <james.longfrenchname@gmail.com>
2025-03-24 12:20:28 -0700

    finished a lesson

--- P_ThanosSnap.cs
+++ P_ThanosSnap.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assets.code; // Make sure that this is always here. 
 
 namespace ShadowsThanosSnap // Annotations from the DLL Modding Guide on SOFG Wiki. 
 {
-    public class P_EyesInShadow : Power // We are creating a new class, P_EyesInShadow, which is a subclass of the Power class. A class is a type of object that can store properties, or other data, and perform functions. "Public" means that this class can be accessed from other classes. Usually a class should be private, but in SOFG a private class will be reset every time that the game is reloaded, and we don't usually want that. 
+    public class P_ThankosSnap : Power // We are creating a new class, P_EyesInShadow, which is a subclass of the Power class. A class is a type of object that can store properties, or other data, and perform functions. "Public" means that this class can be accessed from other classes. Usually a class should be private, but in SOFG a private class will be reset every time that the game is reloaded, and we don't usually want that. 
     {
-        public P_EyesInShadow(Map map) // This is a constructor, which means that this function is called when a new object (or instance) of this class is created. 
+        public P_ThanosSnap(Map map) // This is a constructor, which means that this function is called when a new object (or instance) of this class is created. 
         // The "Map" in the parentheses is a parameter, which is a variable that is passed to the function when it is called. "Map map" refers to a Map (UP) called map (lc).
             : base(map)
         // ": base" means that the constructor will do things in the parent class (Power) before doing its own thing. Like other constructors, it needs a Map in order to do anything. If the first parens had been "(Map dolittle)" then this would have read ": base(dolittle)" instead
         {   // Because the curly brackets are empty, this constructor only does things that are done in the parent class (which is Power). 
         }
 
-        public override string getName()
+        public /* Can be used by things outside P_EyesInShadows or Power */ override /* Replaces the "return 'Default Name' " function from the Power Class */ string /* function will return a string */ getName() // Like JS, functions need parentheses even if there's nothing in them.
         {
-            return "Eyes in the Shadows";
+            return "Eyes in the Shadows"; // Make sure to always have semicolons at the end of lines in C#.
         }
 
         public override string getDesc()
@@ -27,7 +28,7 @@ namespace ShadowsThanosSnap // Annotations from the DLL Modding Guide on SOFG Wi
 
         public override Sprite getIconFore()
         {
-            return map.world.iconStore.enshadow;
+            return map.world.iconStore.enshadow; // "Eyes in the Shadows" uses the same icon as "Enshadow."
         }
 
         public override bool validTarget(Unit unit)
